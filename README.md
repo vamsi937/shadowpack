54513c0b2412ed2f29aca4d1a4be3e0984a456cd
vamsi937 <konchadavamsikrishna0602@gmail.com>
2021-07-04 00:15:28 +0530

    [new:] Adding padding_distance, shadow_color, shadow_animate, shadow_animation_duration options

--- README.md
+++ README.md
@@ -13,14 +13,22 @@ import {shadowpacker} from 'shadowpacker';
 
 shadowpacker({
     shadow_type:'soft',
-    padding:false
+    padding:true,
+    padding_distance:"1.5em",
+    shadow_color:"#ddd",
+    shadow_animate:true,
+    shadow_animation_duration:5
 });
 
 ```
 
 ## Options
 
-shadowpacker supports 2 options, both of which are optional:
+shadowpacker supports 6 options, both of which are optional:
 
 - _shadow_type_ - _hard | soft_ (Defaults to soft)
 - _padding_ - _boolean_ (Defaults to false)
+- _padding_distance_ - _valid distance unit_ (Defaults to 1em) (to be provided only when padding option is true)
+- _shadow_color_ - _valid rgba or hex color_ (Defaults to rgba( 0, 0, 0, 0.12))
+- _shadow_animate_ - _boolean_ (Defaults to false)
+- _shadow_animation_duration_ - _time in secs_ (Defaults to 1 sec) (to be provided only when shadow_animate option is true)
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function shadowpacker(options) {
   let images = document.querySelectorAll(".shadowpacker");
+  let shadowColor = !options.shadow_color
+    ? "rgba( 0, 0, 0, 0.12)"
+    : options.shadow_color;
 
   if (options.shadow_type === "hard") {
     options.shadow_type = "0px";
@@ -8,12 +11,32 @@ function shadowpacker(options) {
   }
 
   images.forEach((image) => {
-    image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba( 0, 0, 0, 0.12)`;
+    image.style.boxShadow = `10px 10px ${options.shadow_type} 1px ${shadowColor}`;
 
     if (options.padding) {
-      image.style.padding = "1em";
+      image.style.padding = !options.padding_distance
+        ? "1em"
+        : options.padding_distance;
+    }
+
+    if (options.shadow_animate) {
+      let darkShadow = true;
+      window.setInterval(
+        function () {
+          image.style.boxShadow = `10px 10px ${options.shadow_type} 1px ${
+            darkShadow ? "#ccc" : "#000"
+          }`;
+          darkShadow = !darkShadow;
+        },
+        !options.shadow_animation_duration
+          ? 1000
+          : options.shadow_animation_duration * 1000
+      );
     }
   });
 }
+
+//shadow_animate
+//shadow_animation_duration
 
 module.exports.shadowpacker = shadowpacker;
